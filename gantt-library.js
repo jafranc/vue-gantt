21ea00b9e9b9d2101aa334aea48584bdd8db44c4
jacques FRANC <jacquesn7@gmail.com>
2025-10-24 16:58:11 +0200

    install in Vue

--- gantt-library.js
+++ gantt-library.js
@@ -1,6 +1,12 @@
 import GanttChart from './src/components/GanttChart.vue';
 
 // Nous exportons le composant pour qu'il puisse être utilisé par d'autres applications.
-export { GanttChart };
+// export { GanttChart };
+
+export default {
+    install(Vue) {
+        Vue.component('GanttChart', GanttChart);
+    }
+};
 
 // En mode UMD, cela permettra d'accéder à 'GanttChart' via la variable globale 'GanttLib'.
